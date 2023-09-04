2746c7761c0f1af4551cfcd2c0f02f0846edc934
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-04 22:35:14 +0530

    navbar changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './component/NavBar/NavBar'
+import Content from './component/Content/Content'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Content/>
     </div>
   )
 }
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './NavBar.module.css';
 import { AiFillHome } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
 import { BsPersonCircle } from "react-icons/bs";
+import { FiMenu } from "react-icons/fi";
+import { FiXCircle } from "react-icons/fi";
+import { ImCross } from "react-icons/im";
+
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    if(show){
+      setShow(true)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('click', handleClick);
+    return () => {
+      window.removeEventListener('click', handleClick)
+    }
+  }, [])
   return (
     <div className={style.mainContainer}>
       <div className={style.navContainer}>
         <div className={style.logo}>
-          <img src="/logo.png" className='img img-fluid' width={150} height={100} alt="logo" />
+          <img src="/logo.png" className='img img-fluid' alt="logo" width={120} height={40}/>
         </div>
         <div className={style.navList}>
           <div className={style.navListItem}>
-            <a href="#"><AiFillHome style={{color:"black", width:'20px', height:'20px'}}/></a>
+            <a href="#"><AiFillHome style={{ color: "black", width: '20px', height: '20px' }} /></a>
           </div>
           <div className={`${style.navListItem} ${style.navListCart}`}>
             <a href="#"><BsFillCartFill style={{ color: "black", width: '20px', height: '20px' }} /></a>
-            <badge className= { style.cartBadge}>
-              1
+            <badge className={style.cartBadge}>
+              {1}
             </badge>
           </div>
           <div className={style.navListItem}>
             <a href="#"><BsPersonCircle style={{ color: "black", width: '20px', height: '20px' }} /></a>
+          </div>
+        </div>
+        <div className={style.toggleMenu}>
+          <div className={style.toggleMenuItem}
+            onClick={() => setShow(!show)}
+          >
+     
+              <FiMenu id='menuIconHandburg' style={{ color: "black", width: '20px', height: '20px' }} />
+          </div>
+        </div>
+      </div>
+      {/* Off canvas section */}
+      <div className={`${show ? style.mainNavListConatinerAfter : style.mainNavListConatinerBefore}`}>
+        <div className={style.toggleMenuList}>
+
+          <div className={style.toggleMenuListItems}>
+            <AiFillHome /> Home
+          </div>
+          <div className={style.toggleMenuListItems}>
+            <BsFillCartFill /> Cart
+          </div>
+          <div className={style.toggleMenuListItems}>
+            <BsPersonCircle /> Profile
           </div>
         </div>
       </div>
