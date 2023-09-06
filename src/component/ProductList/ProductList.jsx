b58573cae540d77a509528478114839e86722fb8
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-06 10:36:15 +0530

    tooltip changed

--- src/component/ProductList/ProductList.jsx
+++ src/component/ProductList/ProductList.jsx
@@ -1,24 +1,71 @@
 import React from 'react'
-import style from './ProductList.module.css'
+import style from './ProductList.module.css';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 const ProductList = () => {
     const imageWidth = "150px"
     const titleWidth = "220px"
+
     return (
         <div className={style.mainContainer}>
             {/* product list container contain each product */}
             <div className={`card ${style.cardItem}`}>
                 <div className={style.imageContainer}>
                     <div className={style.cartIcon}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16" data-tooltip-id="tooltip-1">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                        
+                    </div>
+                    <img src="/shirt1.jpg" class="card-img-top" alt="Product Image" style={{ width: imageWidth, height: "auto", objectFit: "cover" }} />
+                </div>
+                <ReactTooltip
+                    id='tooltip-1'
+                    position='top'
+                    content='add to basket'
+                />
+                <div className={`card-body ${style.cardBody}`}>
+                    <p class="card-text pt-0" style={{ wordWrap: "break-word", maxWidth: titleWidth, fontSize: '15px' }}>Adlor Trendy Cotton Round Neck Regular Fit T Shirt for Men-Elegant Blue</p>
+                    <p class="text-muted">Men's Apparels</p>
+                    <p class="card-text text-danger pt-0" style={{ fontSize: "30px" }}>₹ 99.99</p>
+                    <div class="btn-group mt-0 pt-0" role="group" id={style.buttonList}>
+                        <button type="button" class="btn btn-outline-danger">M</button>
+                        <button type="button" class="btn btn-outline-danger">L</button>
+                        <button type="button" class="btn btn-outline-danger">XL</button>
+                    </div>
+                    <div class="mt-2">
+                        <p class="card-text">
+                            <span class="rating">
+                                <i class="fa fa-star checked text-danger"></i>
+                                <i class="fa fa-star checked text-danger"></i>
+                                <i class="fa fa-star checked"></i>
+                                <i class="fa fa-star checked"></i>
+                                <i class="fa fa-star"></i>
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`card ${style.cardItem}`}>
+               
+                <div className={style.imageContainer}>
+                    <div className={style.cartIcon}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16" data-tooltip-id='tooltip-2'>
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
                     </div>
-                    <img src="/shirt1.jpg" class="card-img-top" alt="Product Image" style={{ width: imageWidth, height: "auto", objectFit: "cover"}} />
+                    <img src="/shirt2.jpg" class="card-img-top" alt="Product Image" style={{ width: imageWidth, height: "auto", objectFit: "cover" }} />
+                   
                 </div>
-                <div className={`card-body ${style.cardBody}`}> 
-                    <p class="card-text pt-0" style={{wordWrap:"break-word", maxWidth:titleWidth, fontSize:'15px'}}>Adlor Trendy Cotton Round Neck Regular Fit T Shirt for Men-Elegant Blue</p>
+                <ReactTooltip
+                    id='tooltip-2'
+                    position='top'
+                    content='add to basket'
+                />
+                <div className={`card-body ${style.cardBody}`}>
+                    <p class="card-text pt-0" style={{ wordWrap: "break-word", maxWidth: titleWidth, fontSize: '15px' }}>Adlor Trendy Cotton Round Neck Regular Fit T Shirt for Men-Elegant Blue</p>
                     <p class="text-muted">Men's Apparels</p>
-                    <p class="card-text text-danger pt-0" style={{fontSize:"30px"}}>₹ 99.99</p>
+                    <p class="card-text text-danger pt-0" style={{ fontSize: "30px" }}>₹ 123.00</p>
                     <div class="btn-group mt-0 pt-0" role="group" id={style.buttonList}>
                         <button type="button" class="btn btn-outline-danger">M</button>
                         <button type="button" class="btn btn-outline-danger">L</button>
@@ -41,44 +88,17 @@ const ProductList = () => {
             <div className={`card ${style.cardItem}`}>
                 <div className={style.imageContainer}>
                     <div className={style.cartIcon}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                        </svg>
-                    </div>
-                    <img src="/shirt2.jpg" class="card-img-top" alt="Product Image" style={{ width: imageWidth, height: "auto", objectFit: "cover"}} />
-                </div>
-                <div className={`card-body ${style.cardBody}`}> 
-                    <p class="card-text pt-0" style={{wordWrap:"break-word", maxWidth:titleWidth, fontSize:'15px'}}>Adlor Trendy Cotton Round Neck Regular Fit T Shirt for Men-Elegant Blue</p>
-                    <p class="text-muted">Men's Apparels</p>
-                    <p class="card-text text-danger pt-0" style={{fontSize:"30px"}}>₹ 123.00</p>
-                    <div class="btn-group mt-0 pt-0" role="group" id={style.buttonList}>
-                        <button type="button" class="btn btn-outline-danger">M</button>
-                        <button type="button" class="btn btn-outline-danger">L</button>
-                        <button type="button" class="btn btn-outline-danger">XL</button>
-                    </div>
-                    <div class="mt-2">
-                        <p class="card-text">
-                            <span class="rating">
-                                <i class="fa fa-star checked text-danger"></i>
-                                <i class="fa fa-star checked text-danger"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star"></i>
-                            </span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div className={`card ${style.cardItem}`}>
-                <div className={style.imageContainer}>
-                    <div className={style.cartIcon}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16" data-tooltip-id='tooltip-3'>
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
                     </div>
                     <img src="/shirt3.jpg" class="card-img-top" alt="Product Image" style={{ width: imageWidth, height: "auto", objectFit: "cover" }} />
                 </div>
+                <ReactTooltip
+                    id='tooltip-3'
+                    position='top'
+                    content='add to basket'
+                />
                 <div className={`card-body ${style.cardBody}`}>
                     <p class="card-text pt-0" style={{ wordWrap: "break-word", maxWidth: titleWidth, fontSize: '15px' }}>Adlor Trendy Cotton Round Neck Regular Fit T Shirt for Men-Elegant Blue</p>
                     <p class="text-muted">Men's Apparels</p>
@@ -101,7 +121,7 @@ const ProductList = () => {
                     </div>
                 </div>
             </div>
-            
+
         </div>
 
     )
